a4e141bda54a01cdfd12d3546bedb0bc67884254
natefduncan <natefduncan@gmail.com>
2019-08-08 17:00:00 -0500

    final edits on initial

--- cado_django/cado_django/rest_api/views.py
+++ cado_django/cado_django/rest_api/views.py
@@ -1,20 +1,18 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewset
+from rest_framework import viewsets
 from cado_django.rest_api.serializers import UserSerializer, GroupSerializer
 
-from django.shortcuts import render #This was here by default.
-
-class UserViewSet(viewset.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited
     """
-    queryset = User.object.all().order_by('-date-joined')
+    queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
 class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited
     """
-    queryset = Group.object.all()
+    queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
--- cado_django/cado_django/urls.py
+++ cado_django/cado_django/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.urls import include, path
 from rest_framework import routers
-from rest_api import views
+from cado_django.rest_api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
