5fae5c07d5df789f796561d9c8c5a8533dfc1e82
natefduncan <natefduncan@gmail.com>
2019-08-08 16:54:20 -0500

    directory edits

--- cado_django/cado_django/settings.py
+++ cado_django/cado_django/settings.py
@@ -28,9 +28,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 #Rest Framework settings
-INSTALLED_APPS = {
+REST_FRAMEWORK = {
         'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination', 
-        'PAGE_SIZE' : 10
+        'PAGE_SIZE' : 10,
+        'DEFAULT_PERMISSION_CLASSES' : 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         }
 
 
@@ -43,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- cado_django/cado_django/urls.py
+++ cado_django/cado_django/urls.py
@@ -13,15 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
+from rest_api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls'), namespace='rest_framework'))
+    path(r'^', include(router.urls)),
+    path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
